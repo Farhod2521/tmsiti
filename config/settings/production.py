2fd1be80f5d7ce1ec97fe9bd395c6abbcf7faf1e
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-03-14 17:09:12 +0500

    fixed

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,7 +2,7 @@
 from .base import *
 import os
 from dotenv import load_dotenv
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -22,7 +22,11 @@ DATABASES = {
         }
     }
 }
-
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+X_FRAME_OPTIONS = 'DENY'
 
 
 
