e8102c4b09aef82a056040f4fea4e1758f1f7049
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-03-26 10:16:53 +0500

    fixed

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import subsystem, shnk, shnkgroup
 
 app = FastAPI(title="FastAPI SHNK API", version="1.0")
 
+# CORS middleware qo'shish
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Barcha domenlarga ruxsat
+    allow_credentials=True,
+    allow_methods=["*"],  # Barcha HTTP metodlarga ruxsat
+    allow_headers=["*"],  # Barcha HTTP headerlarga ruxsat
+)
+
 app.include_router(subsystem.router)
 app.include_router(shnk.router)
-#app.include_router(shnkgroup.router)
+# app.include_router(shnkgroup.router)
 
 @app.get("/")
 def root():
