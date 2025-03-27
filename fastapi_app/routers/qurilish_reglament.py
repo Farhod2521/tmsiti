f77b22c3c3c4d2be4ab1636b86537c9301396790
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-03-27 16:39:27 +0500

    fixed

--- fastapi_app/routers/qurilish_reglament.py
+++ fastapi_app/routers/qurilish_reglament.py
@@ -1,11 +1,17 @@
 from schemas.qurilish_reglament import QurilishReglamentSchema
 from typing import Dict, List
 from fastapi import APIRouter, Depends
-
+from database import get_db
 from models import QurilishReglament
 router = APIRouter(prefix="/reglament", tags=["QM"])
 
 
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from fastapi import Depends
+from database import get_db
+
 @router.get("/qurilish/", response_model=List[QurilishReglamentSchema])
-async def get_qurilish():
-    return await QurilishReglament.all()
+async def get_qurilish(session: AsyncSession = Depends(get_db)):
+    result = await session.execute(select(QurilishReglament))
+    return result.scalars().all()
