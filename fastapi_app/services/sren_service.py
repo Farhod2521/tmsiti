acf519403a242cab58348c491578cfd09335a0a0
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-04-24 18:09:18 +0500

    fixed

--- fastapi_app/services/sren_service.py
+++ fastapi_app/services/sren_service.py
@@ -1,26 +1,19 @@
 from typing import List
-from sqlalchemy.orm import Session
-from fastapi import Depends
-from models import SREN, SREN_SHNK  # SQLAlchemy models
+from models import SREN, SREN_SHNK  # Django modeli ishlatilmoqda
 from schemas.sren_schema import SRENSchema, SREN_SHNKSchema
-from database import get_db  # Dependency to get DB session
 
-def get_all_sren(db: Session) -> List[SRENSchema]:
+def get_all_sren() -> List[SRENSchema]:
     sren_list = []
-    # Querying all SREN records
-    sren_records = db.query(SREN).all()
-
-    for sren in sren_records:
-        # Querying related SREN_SHNK records
-        shnk_list = [
-            SREN_SHNKSchema(
-                sren_shnk_uz=shnk.name,
-                sren_shnk_ru=shnk.name,
-                sren_designation=shnk.designation
-            ) for shnk in db.query(SREN_SHNK).filter(SREN_SHNK.sren_id == sren.id).all()
-        ]
-        
-        # Creating SRENSchema instance
+    for sren in SREN.objects.all():
+        shnk_list = SREN_SHNKSchema.from_orm_list(
+            [
+                SREN_SHNKSchema(
+                    sren_shnk_uz=shnk.name,
+                    sren_shnk_ru=shnk.name,
+                    sren_designation=shnk.designation
+                ) for shnk in SREN_SHNK.objects.filter(sren=sren)
+            ]
+        )
         sren_schema = SRENSchema(
             sren_name_uz=sren.name,
             sren_name_ru=sren.name,
@@ -28,5 +21,4 @@ def get_all_sren(db: Session) -> List[SRENSchema]:
             sren_shnk=shnk_list
         )
         sren_list.append(sren_schema)
-    
     return sren_list
