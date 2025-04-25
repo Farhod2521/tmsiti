2df89806493d8645a70b22962f408342f9405663
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-04-25 11:50:23 +0500

    fixed

--- fastapi_app/schemas/sren_schema.py
+++ fastapi_app/schemas/sren_schema.py
@@ -5,8 +5,7 @@ class SREN_SHNKSchema(BaseModel):
     sren_shnk_uz: str
     sren_shnk_ru: str
     sren_designation: str
-    sren_pdf_uz: Optional[str] = None
-    sren_pdf_ru: Optional[str] = None
+
     class Config:
         orm_mode = True
 
@@ -14,6 +13,8 @@ class SRENSchema(BaseModel):
     sren_name_uz: str
     sren_name_ru: str
     sren_designation: str
+    sren_pdf_uz: Optional[str] = None
+    sren_pdf_ru: Optional[str] = None
     sren_shnk: List[SREN_SHNKSchema] = []
 
     class Config:
