bd21b196e7ba4d056eda87ff546e65bd3dae8f08
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-04-25 11:47:32 +0500

    fixed

--- fastapi_app/schemas/sren_schema.py
+++ fastapi_app/schemas/sren_schema.py
@@ -1,11 +1,12 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List,Optional
 
 class SREN_SHNKSchema(BaseModel):
     sren_shnk_uz: str
     sren_shnk_ru: str
     sren_designation: str
-
+    sren_pdf_uz: Optional[str] = None
+    sren_pdf_ru: Optional[str] = None
     class Config:
         orm_mode = True
 
