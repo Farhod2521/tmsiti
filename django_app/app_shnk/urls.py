4292104ec310c6ccd4b2a7e44a93eb1a3a17212b
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-12-02 10:46:14 +0500

    fixed

--- django_app/app_shnk/urls.py
+++ django_app/app_shnk/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from .views import TexnikReglaamentListAPIView
+from .views import TexnikReglaamentListAPIView, StandardPdfToImagesAPIView
 
 urlpatterns = [
     path("texnik-reglament/", TexnikReglaamentListAPIView.as_view(), name="texnik-reglament-list"),
+    path("standard/<slug:slug>/images/", StandardPdfToImagesAPIView.as_view())
 ]
--- django_app/app_shnk/views.py
+++ django_app/app_shnk/views.py
@@ -4,11 +4,50 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Texnik_reglaament
+from .models import Texnik_reglaament, Standard
 from .serializers import TexnikReglaamentSerializer
+
+
+import base64
+from io import BytesIO
+from pdf2image import convert_from_path
 
 class TexnikReglaamentListAPIView(APIView):
     def get(self, request, *args, **kwargs):
         queryset = Texnik_reglaament.objects.all()
         serializer = TexnikReglaamentSerializer(queryset, many=True, context={"request": request})
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+
+class StandardPdfToImagesAPIView(APIView):
+    """
+    PDF sahifalarini rasmga aylantirib, SAQLAMASDAN base64 ko‘rinishda qaytaradi.
+    """
+
+    def get(self, request, slug):
+        try:
+            standard = Standard.objects.get(slug=slug)
+        except Standard.DoesNotExist:
+            return Response({"detail": "Hujjat topilmadi."}, status=status.HTTP_404_NOT_FOUND)
+
+        pdf_path = standard.pdf.path
+
+        try:
+            # RAM ichida PDF sahifalarni o‘qish
+            pages = convert_from_path(pdf_path, dpi=180)
+        except Exception as e:
+            return Response({"detail": f"PDFni o‘qishda xatolik: {str(e)}"}, status=500)
+
+        images_base64 = []
+
+        for page in pages:
+            buffer = BytesIO()
+            page.save(buffer, format="JPEG")
+            encoded = base64.b64encode(buffer.getvalue()).decode("utf-8")
+            images_base64.append(f"data:image/jpeg;base64,{encoded}")
+
+        return Response({
+            "slug": slug,
+            "page_count": len(images_base64),
+            "images": images_base64
+        })
